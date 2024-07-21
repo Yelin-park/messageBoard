f3f8b8a9a4416350636ca5c55c7cc9dd334d82ac
Yaliny <mae1217@naver.com>
2024-07-21 23:04:58 +0900

    feat(게시글 등록 화면): 구현 및 API 연동 완료

---
+++ src/test/java/com/yaliny/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.yaliny.book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPage_loading() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("Welcome To Yaliny World");
+    }
+}
