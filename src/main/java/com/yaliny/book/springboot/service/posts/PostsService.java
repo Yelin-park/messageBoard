5c2b3e3ab8070a86a8f1025c6c9b07fe9e390f00
Yaliny <mae1217@naver.com>
2024-07-21 23:27:02 +0900

    feat(게시글 조회 화면): 구현 및 API 연동 완료

--- src/main/java/com/yaliny/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yaliny/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.yaliny.book.springboot.service.posts;
 
 import com.yaliny.book.springboot.domain.posts.Posts;
 import com.yaliny.book.springboot.domain.posts.PostsRepository;
+import com.yaliny.book.springboot.web.dto.PostsListResponseDto;
 import com.yaliny.book.springboot.web.dto.PostsResponseDto;
 import com.yaliny.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yaliny.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,8 +34,16 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+            .collect(Collectors.toList());
     }
 }
