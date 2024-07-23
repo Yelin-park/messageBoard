6e2fd44e2beb24923b7d66c1421699673b533b2f
Yaliny <mae1217@naver.com>
2024-07-23 21:22:05 +0900

    feat(구글 로그인): 구현 완료
    세션값 가져오는 부분 어노테이션 기반으로 리팩토링

---
+++ src/main/java/com/yaliny/book/springboot/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.yaliny.book.springboot.config;
+
+import com.yaliny.book.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * LoginUserArgumentResolver이 스프링에서 인식될 수 있도록 설정 추가
+ * */
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
